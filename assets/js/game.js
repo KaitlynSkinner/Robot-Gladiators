b7b17bc6d446aff56197cc7be2be84b77a2838f3
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-01-10 10:57:29 -0500

    Add Enemy Combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,13 +4,14 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 //You can also log multiple values at once like thuis
-console.log(playerName, playerAttack, playerHealth);
+//console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack= 12;
 
-var fight = function() {
+var fight = function(enemyName) {
+    // Fight function statements
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     // If player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -55,31 +56,32 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
       fight();
     }
   }
+/*
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
-
-    // Subtract the value of 'playerAttack' from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-    enemyHealth = enemyHealth - playerAttack;
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
-    );
-    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-        playerHealth = playerHealth - enemyAttack;
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
-    );
+*/
 };
 
-// fight();
-    // Game States
-    // "WIN" - Player robot has defeated all enemy-robots
-    //  * Fight all enemy-robots
-    //  * Defeat each enemy-robot
-    // "LOSE" - Player robot's health is zero or less
-var enemy1 = "Roberto";
-var enemy2 = "Amy Android";
+// fight(); ..now being replaced by below for loop
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+
+var enemy1 = "Roberto"
+var enemy2 = "Amy Adroid";
 var enemy3 = "Robo Trumble";
 
-console.log(enemyNames);
+//console.log(enemyNames.length);
+
+//for(var i = 0; i < enemyNames.length; i++) {
+  //console.log(enemyNames[i]);
+  //console.log(i);
+  //console.log(enemyNames[i] + " is at " + i + " index");
+//}
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//  * Fight all enemy-robots
+
+//  * Defeat each enemy-robot
+
+// "LOSE" - Player robot's health is zero or less
